fa7c7ce1c2a4a38711fda259c38245656ac656b5
Spyridon Alfredos Desyllas <sadesyllas@gmail.com>
2017-02-09 00:01:53 +0200

    Initialize repository

---
+++ lib/sql_sp_cache.ex
@@ -0,0 +1,18 @@
+defmodule SQLSPCache do
+  @moduledoc """
+  Documentation for SQLSPCache.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SQLSPCache.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sql_sp_cache_test.exs
@@ -0,0 +1,8 @@
+defmodule SQLSPCacheTest do
+  use ExUnit.Case
+  doctest SQLSPCache
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
